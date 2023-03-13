6a42a728c51d3dc1654e6f7287e662b2eb43f313
momonastia~ <anastasiatyrikova@gmail.com>
2023-03-14 00:59:43 +0100

    Feat: menu icons added

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,18 +1,29 @@
 import { CiPizza } from "react-icons/ci";
+import { BiFoodMenu, BiHomeAlt } from "react-icons/bi";
+import { MdOutlineRateReview, MdOutlineContactSupport } from "react-icons/md";
 import Link from "next/link";
 import styles from "./styles.module.scss";
 
 const Header = () => {
   return (
     <header>
-      <div>
+      <div className={styles.logo}>
         <CiPizza />
       </div>
       <nav className={styles.NavBar}>
-        <Link href="/">Home</Link>
-        <Link href="/about">About us</Link>
-        <Link href="/reviews">Reviews</Link>
-        <Link href="/products">Our menu</Link>
+        <Link href="/">
+          <BiHomeAlt /> Home
+        </Link>
+        <Link href="/products">
+          <BiFoodMenu /> Our menu
+        </Link>
+        <Link href="/reviews">
+          <MdOutlineRateReview />
+          Reviews
+        </Link>
+        <Link href="/about">
+          <MdOutlineContactSupport /> About us
+        </Link>
       </nav>
     </header>
   );
