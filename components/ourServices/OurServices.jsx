0b58f80beceac1d54eb097c7d7a6cebf517d563a
momonastia~ <anastasiatyrikova@gmail.com>
2023-03-25 23:50:43 +0100

    Chore: our services desktop styles added

--- components/ourServices/OurServices.jsx
+++ components/ourServices/OurServices.jsx
@@ -1,36 +1,35 @@
-import { TbChefHat, TbSalad, TbTruckDelivery } from "react-icons/tb";
-/* import styles from ".//styles.module.scss"; */
+import { GiCook, GiTomato } from "react-icons/gi";
+import { MdDeliveryDining } from "react-icons/md";
+import styles from ".//styles.module.scss";
 
 const OurServices = () => {
   return (
-    <>
-      <section>
+    <div className={styles.ServicesContainer}>
+      <section className={styles.ServicesTitle}>
         <h2>Our services</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo odit,
-          tempore laudantium dolorum molestiae, officia obcaecati aspernatur,
-          adipisci magnam deserunt voluptatibus iusto? Rem explicabo facilis,
-          quibusdam suscipit quas doloremque odit.
+          tempore laudantium dolorum molestiae.
         </p>
       </section>
-      <section>
-        <div>
-          {/*  <TbChefHat /> */}
+      <section className={styles.ServicesList}>
+        <div className={styles.ServicesListItem}>
+          <GiCook className={styles.ServicesIcon} />
           <h3>Profecional chiefs</h3>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
         </div>
-        <div>
-          {/*   <TbSalad /> */}
+        <div className={styles.ServicesListItem}>
+          <GiTomato className={styles.ServicesIcon} />
           <h3>Fresh ingredients</h3>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
         </div>
-        <div>
-          {/*     <TbTruckDelivery /> */}
+        <div className={styles.ServicesListItem}>
+          <MdDeliveryDining className={styles.ServicesIcon} />
           <h3>Fast delivery</h3>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
