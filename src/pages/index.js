d0dfcc0b61cf9ded9d7b96f0df859503d569d9ed
momonastia~ <anastasiatyrikova@gmail.com>
2023-03-27 23:58:55 +0200

    Fix folders for deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         <div className={styles.mainImageContainer}>
           <Image
             className={styles.mainImage}
-            src="/../public/images/pizzaHero.jpg"
+            src="/../public/static/images/pizzaHero.jpg"
             alt="pizza"
             width={1000}
             height={500}
