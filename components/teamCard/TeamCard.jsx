7ac37d904aac9802a2324f2445630a403672b09b
momonastia~ <anastasiatyrikova@gmail.com>
2023-04-07 14:36:54 +0200

    Fix: forwarsref error for deploy

--- components/teamCard/TeamCard.jsx
+++ components/teamCard/TeamCard.jsx
@@ -3,6 +3,7 @@ import { forwardRef } from "react";
 import styles from "./styles.module.scss";
 import { motion } from "framer-motion";
 
+// eslint-disable-next-line react/display-name
 const TeamCard = forwardRef(({ member }, ref) => {
   return (
     <div className={styles.teamCard} ref={ref}>
