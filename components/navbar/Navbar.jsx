d0d8c991e669fe2dce9351c8ec1f421a909e25e9
anastasia tuyrikova <anastasiatyrikova@gmail.com>
2023-07-03 18:43:33 +0200

    test

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -76,6 +76,7 @@ const Navbar = () => {
       <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
+      <p>test</p>
     </div>
   );
 };
